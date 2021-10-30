fe5ea9195c5c4e44e889871febb4a7e57e384621
Gary <williamsg12>
2021-10-30 19:13:04 -0400

    added stuff

--- portfolio/src/Components/Resume.js
+++ portfolio/src/Components/Resume.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Resume = () => {
     return (
-        <div>
-            
-        </div>
-    );
+			<div>
+				
+			</div>
+		);
 };
 
 export default Resume;
